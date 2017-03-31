f0460a007827fde64f4178629d7048c98cd27a5e
Tormod Haugland <tormod.haugland@gmail.com>
2017-03-31 08:31:56 +0200

    Added word model

--- words/models.py
+++ words/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=1000)
+    freeTrans = models.CharField(max_length=1000)
+    freeTrans2 = models.CharField(max_length=1000)
+    comment = models.TextField()
